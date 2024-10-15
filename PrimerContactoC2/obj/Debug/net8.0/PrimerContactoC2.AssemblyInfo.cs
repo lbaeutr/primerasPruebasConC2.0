1b2b6f456a9a32b096507b7187ebfb393b03d85d
lbaeutr <lbaeutr@gmail.com>
2024-10-15 13:46:46 +0200

    Breves cambios

--- PrimerContactoC2/obj/Debug/net8.0/PrimerContactoC2.AssemblyInfo.cs
+++ PrimerContactoC2/obj/Debug/net8.0/PrimerContactoC2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrimerContactoC2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88880414243912867a50c94c8d4851c254b2e528")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrimerContactoC2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrimerContactoC2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
